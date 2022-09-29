0d6ddd3517ccc55e8cc9585059bbad4e1bc7f84b
sahyunjin <tkguswls1106@naver.com>
2022-09-30 00:36:25 +0900

    기본 gradle 프로젝트 생성 완료

---
+++ src/main/java/shjgroup/travelclubcoregrd/TravelclubCoreGrdApplication.java
@@ -0,0 +1,13 @@
+package shjgroup.travelclubcoregrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelclubCoreGrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelclubCoreGrdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shjgroup/travelclubcoregrd/TravelclubCoreGrdApplicationTests.java
@@ -0,0 +1,13 @@
+package shjgroup.travelclubcoregrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelclubCoreGrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
